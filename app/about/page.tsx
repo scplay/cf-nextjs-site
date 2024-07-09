09dcb509f1190dff2e70b5301730ff7a86408405
zeonwang <scplay@gmail.com>
2024-07-09 18:42:30 +0800

    add contact me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <h1>About Zeon</h1>
+      <h1><a href="https://cv.zeon.wang/cv/">Contact Me</a></h1>
       <p>Constructing</p>
       <div>
         Back to <Link href="/">Home</Link>
