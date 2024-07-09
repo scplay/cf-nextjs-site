04ad348d15f848ce224579a4ebf970a16ff8fac8
zeonwang <scplay@gmail.com>
2024-07-09 20:41:25 +0800

    update home add typed.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+"use client"
+
 import Link from "next/link";
+import { useEffect, useRef } from "react";
+import Typed from 'typed.js';
 
 const Home = () => {
+
+  const termEl = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(termEl.current, {
+      strings: [
+        'Hi! this is ZeonWang, a developer.',
+        'Hi! this is ZeonWang, a designer.',
+        'Hi! this is ZeonWang, a starcraft player.',
+      ],
+      
+      // cursorChar: '_',
+      backSpeed: 0,
+      typeSpeed: 40,
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div>
-      <h1>Hi! This is ZeonWang, a developer</h1>
+      <h1 style={{height: '45px'}} ><span ref={termEl} /></h1>
       <p>
         Visit the <Link href="/about">About</Link> page.
       </p>
