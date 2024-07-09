cad89473f21f34b0c1c5105a7fcc8f1f0767073f
zeonwang <scplay@gmail.com>
2024-07-09 18:49:18 +0800

    update contact me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <h1><a href="https://cv.zeon.wang/cv/">Contact Me</a></h1>
-      <p>Constructing</p>
+      <h1><a href="https://cv.zeon.wang/cv/">About Me</a></h1>
+      <p>Mobile/WeChat: <a href="tel:15622209229">15622209229</a></p>
+      <p>Email: <a href="mailto:scplay@gmail.com" >scplay@gmail.com</a></p>
       <div>
         Back to <Link href="/">Home</Link>
       </div>
